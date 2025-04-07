d5bba0474ba9bbff60ff1fde90b2acb425261d58
Snehil Gupta <snehil.gupta@comviva.com>
2025-04-07 12:54:31 +0530

    added basic auth

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return  http.build();
+    }
+
+//    //  Define an in-memory user
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("user")
+//                        .password(passwordEncoder().encode("pass123"))
+//                        .roles("USER")
+//                        .build()
+//        );
+//    }
+//
+//    // 👇 Define password encoder
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+
+}
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/dto/CommentDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/dto/CommentDto.java
@@ -6,8 +6,8 @@ package com.springboot.blog.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
-import javax.validation.constraints.Size;
 
 public class CommentDto {
 
